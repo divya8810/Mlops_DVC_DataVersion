953fc9323c9241a747554e45d527154b495bcdec
Divya <divyagupta989772@gmail.com>
2025-07-29 17:00:25 +0530

    2nd ver save

--- mycode.py
+++ mycode.py
@@ -5,9 +5,11 @@ data={"name":['a','b','c'],
       'Age':[12,34,67],
       'city':['kanpur','delhi','mumbai']
       }
-
-
 df=pd.DataFrame(data)
+new_row={'name':'GINI','Age':20,'city':'agra'}
+df.loc[len(df.index)]=new_row
+
+
 data_dir='data'
 os.makedirs(data_dir,exist_ok=True)
 file_path=os.path.join(data_dir,'sample_data.csv')
